0a6b48b5eef4b02a4649eb8915bed479299cae12
nurks <34299229+nurks@users.noreply.github.com>
2021-12-22 07:55:55 +0600

    Update fooditem.js

--- shoppingsite-main/fooditem.js
+++ shoppingsite-main/fooditem.js
@@ -2,7 +2,7 @@ const foodItem= [
     {
     id: 1,
     name: 'СИЛА В МЫСЛИ.',
-    category : 'biryani',
+
     rating : 4.1,
     price: 2550,
     img: 'images/best-seller/foto1.jpg',
@@ -11,7 +11,7 @@ const foodItem= [
 {
     id: 2,
     name: 'СИСТЕМА ХОДИ',
-    category : 'biryani',
+
     rating : 4.5,
     price: 3015,
     img: 'images/best-seller/foto2.jpg',
@@ -20,7 +20,7 @@ const foodItem= [
 {
     id: 3,
     name: 'РАБОТА ПО ЛЮБВИ',
-    category : 'biryani',
+  
     rating : 4.1,
     price: 1800,
     img: 'images/best-seller/foto3.jpg',
@@ -29,7 +29,7 @@ const foodItem= [
 {
     id: 4,
     name: 'ЦЕЛЬ: ПРОЦЕСС',
-    category : 'biryani',
+
     rating : 4.3,
     price: 1530,
     img: 'images/best-seller/foto4.jpg',
@@ -38,7 +38,7 @@ const foodItem= [
 {
     id: 5,
     name: 'АНТИХРУПКОСТЬ.',
-    category : 'biryani',
+
     rating : 5.1,
     price: 4500,
     img: 'images/best-seller/foto5.jpg',
@@ -47,7 +47,7 @@ const foodItem= [
 {
     id: 6,
     name: 'БЫТЬ ЛИДЕРОМ.',
-    category : 'biryani',
+
     rating : 3.9,
     price: 1250,
     img: 'images/best-seller/foto6.jpg',
@@ -56,7 +56,7 @@ const foodItem= [
 {
     id: 7,
     name: 'ВЕСНУШКА',
-    category : 'biryani',
+
     rating : 3.1,
     price: 13,
     img: 'images/best-seller/foto7.jpg',
@@ -65,7 +65,7 @@ const foodItem= [
 {
     id: 8,
     name: 'ДВАЖДЫ ДВА',
-    category : 'biryani',
+
     rating : 5.3,
     price: 20,
     img: 'images/best-seller/foto8.jpg',
@@ -74,7 +74,7 @@ const foodItem= [
 {
     id: 9,
     name: 'ЛЕД',
-    category : 'biryani',
+ 
     rating : 2.3,
     price: 1490,
     img: 'images/best-seller/foto9.jpg',
@@ -83,7 +83,7 @@ const foodItem= [
 {
     id: 10,
     name: 'ПРИБЕЖИЩЕ',
-    category : 'chicken',
+
     rating : 4.2,
     price: 3190,
     img: 'images/Contemporary prose/foto10.jpg',
@@ -92,7 +92,7 @@ const foodItem= [
 {
     id: 11,
     name: 'ТЕОРИЯ',
-    category : 'chicken',
+
     rating : 4.9,
     price: 3990,
     img: 'images/Contemporary prose/foto11.jpg',
@@ -101,7 +101,7 @@ const foodItem= [
 {
     id: 12,
     name: 'БЕГИ, ИЛИ УМРЕШЬ!',
-    category : 'chicken',
+ 
     rating : 2.3,
     price: 450,
     img: 'images/Contemporary prose/foto12.jpg',
@@ -110,7 +110,7 @@ const foodItem= [
 {
     id: 13,
     name: 'ДРАКОНЬЯ КРОВЬ',
-    category : 'chicken',
+
     rating : 1.9,
     price: 1090,
     img: 'images/Contemporary prose/foto13.jpg',
@@ -119,7 +119,7 @@ const foodItem= [
 {
     id: 14,
     name: 'ТАУН ДАУН',
-    category : 'chicken',
+
     rating : 5.1,
     price: 3700,
     img: 'images/Contemporary prose/foto14.jpg',
@@ -128,7 +128,7 @@ const foodItem= [
 {
     id: 15,
     name: 'СМЕРТЬ',
-    category : 'chicken',
+
     rating : 2.3,
     price: 2000,
     img: 'images/Contemporary prose/foto15.jpg',
@@ -137,7 +137,7 @@ const foodItem= [
 {
     id: 16,
     name: '«НИ ЗЯ.',
-    category : 'paneer',
+ 
     rating : 4.3,
     price: 1590,
     img: 'images/scientific books/foto16.jpg',
@@ -147,7 +147,7 @@ const foodItem= [
 {
     id: 17,
     name: 'Хочу,Mогу,Надо.',
-    category : 'paneer',
+    
     rating : 4.4,
     price: 4500,
     img: 'images/scientific books/foto17.jpg',
@@ -156,7 +156,7 @@ const foodItem= [
 {
     id: 18,
     name: '«НЕ НОЙ.',
-    category : 'paneer',
+
     rating : 4.3,
     price: 15,
     img: 'images/scientific books/foto18.jpg',
@@ -166,7 +166,7 @@ const foodItem= [
 {
     id: 19,
     name: '«Магия утра.',
-    category : 'paneer',
+ 
     rating : 4.3,
     price: 12,
     img: 'images/scientific books/foto19.jpg',
@@ -176,7 +176,7 @@ const foodItem= [
 {
     id: 20,
     name: '«Атлант',
-    category : 'paneer',
+ 
     rating : 4.3,
     price: 8,
     img: 'images/scientific books/foto20.jpg',
@@ -185,7 +185,7 @@ const foodItem= [
 {
     id: 21,
     name: 'Дневник 3',
-    category : 'paneer',
+
     rating : 4.3,
     price: 7,
     img: 'images/scientific books/foto21.jpg',
@@ -194,8 +194,8 @@ const foodItem= [
 {
     id: 22,
     name: 'искусство-пофигизма',
-    age: '32',
-    category : 'paneer',
+
+
     rating : 4.3,
     price: 5,
     img: 'images/scientific books/foto22.jpg',
@@ -204,7 +204,7 @@ const foodItem= [
 {
     id: 23,
     name: ' «Судьба»',
-    category : 'vegetable',
+
     rating : 4.3,
     price: 8,
     img: 'images/fantasy/foto23.jpg',
@@ -213,7 +213,7 @@ const foodItem= [
 {
     id: 24,
     name: 'Последнее желание',
-    category : 'vegetable',
+
     rating : 4.3,
     price: 7,
     img: 'images/fantasy/foto24.jpg',
@@ -222,7 +222,7 @@ const foodItem= [
 {
     id: 25,
     name: 'Игра престолов',
-    category : 'vegetable',
+   
     rating : 4.3,
     price: 5,
     img: 'images/fantasy/foto25.jpg',
@@ -231,7 +231,7 @@ const foodItem= [
 {
     id: 26,
     name: 'Джокер',
-    category : 'vegetable',
+  
     rating : 4.3,
     price: 7,
     img: 'images/fantasy/foto26.jpg',
@@ -240,7 +240,7 @@ const foodItem= [
 {
     id: 27,
     name: '«Дюна»',
-    category : 'vegetable',
+ 
     rating : 4.3,
     price: 10,
     img: 'images/fantasy/foto27.jpg',
@@ -249,7 +249,7 @@ const foodItem= [
 {
     id: 28,
     name: '«Оно»',
-    category : 'vegetable',
+
     rating : 4.3,
     price: 4,
     img: 'images/fantasy/foto28.jpg',
@@ -258,7 +258,7 @@ const foodItem= [
 {
     id: 29,
     name: '«Воин Доброй',
-    category : 'vegetable',
+ 
     rating : 4.3,
     price: 4,
     img: 'images/fantasy/foto29.jpg',
@@ -267,7 +267,7 @@ const foodItem= [
 {
     id: 30,
     name: 'Книги крови',
-    category : 'chinese',
+   
     rating : 4.3,
     price: 8,
     img: 'images/Horror/foto30.jpg',
@@ -276,7 +276,7 @@ const foodItem= [
 {
     id: 31,
     name: '«Багрянец»',
-    category : 'chinese',
+
     rating : 4.3,
     price: 7,
     img: 'images/Horror/foto31.jpg',
@@ -285,7 +285,7 @@ const foodItem= [
 {
     id: 32,
     name: '«Ужас Дэнчика»',
-    category : 'chinese',
+
     rating : 4.3,
     price: 5,
     img: 'images/Horror/foto32.jpg',
@@ -294,7 +294,7 @@ const foodItem= [
 {
     id: 33,
     name: 'Сердце Сумрака',
-    category : 'chinese',
+
     rating : 4.3,
     price: 16,
     img: 'images/Horror/foto33.jpg',
@@ -303,7 +303,7 @@ const foodItem= [
 {
     id: 34,
     name: 'населенный призрак',
-    category : 'chinese',
+
     rating : 4.3,
     price: 14,
     img: 'images/Horror/foto34.jpg',
@@ -312,7 +312,7 @@ const foodItem= [
 {
     id: 35,
     name: 'Мастер ужас',
-    category : 'chinese',
+
     rating : 4.3,
     price: 10,
     img: 'images/Horror/foto35.jpg',
@@ -321,7 +321,7 @@ const foodItem= [
 {
     id: 36,
     name: '«Кэрри»',
-    category : 'chinese',
+
     rating : 4.3,
     price: 8,
     img: 'images/Horror/foto36.jpg',
@@ -330,7 +330,7 @@ const foodItem= [
 {
     id: 37,
     name: '«Институт»',
-    category : 'south indian',
+
     rating : 4.3,
     price: 18,
     img: 'images/Detective/foto37.jpg',
@@ -339,7 +339,7 @@ const foodItem= [
 {
     id: 38,
     name: 'Письма смерти',
-    category : 'south indian',
+
     rating : 4.3,
     price: 20,
     img: 'images/Detective/foto38.jpg',
@@ -348,7 +348,7 @@ const foodItem= [
 {
     id: 39,
     name: 'Жена убийцы',
-    category : 'south indian',
+
     rating : 4.3,
     price: 12,
     img: 'images/Detective/foto39.jpg',
@@ -357,7 +357,7 @@ const foodItem= [
 {
     id: 40,
     name: 'Человек из',
-    category : 'south indian',
+
     rating : 4.3,
     price: 10,
     img: 'images/Detective/foto40.jpg',
@@ -366,7 +366,7 @@ const foodItem= [
 {
     id: 41,
     name: 'OAssassin',
-    category : 'south indian',
+  
     rating : 4.3,
     price: 15,
     img: 'images/Detective/foto41.jpg',
@@ -375,7 +375,7 @@ const foodItem= [
 {
     id: 42,
     name: 'Десять негритят',
-    category : 'south indian',
+
     rating : 4.3,
     price: 40,
     img: 'images/Detective/foto42.jpg',
@@ -384,7 +384,7 @@ const foodItem= [
 {
     id: 43,
     name: 'Ссудный день',
-    category : 'south indian',
+
     rating : 4.3,
     price: 25,
     img: 'images/Detective/foto43.jpg',
@@ -393,7 +393,7 @@ const foodItem= [
 {
     id: 44,
     name: 'Дорога',
-    category : 'south indian',
+
     rating : 4.3,
     price: 34,
     img: 'images/Detective/foto44.jpg',
